dc7cb561160dd1b9691d8b7f4364fed138b1fb15
Hygor Monteiro <112796079+GitHmonteiro@users.noreply.github.com>
2023-04-23 20:05:40 -0300

    Add files via upload
    Um esboço de uma página Inicial de uma loja de time de futebol

---
+++ PaginaFlu/script.js
@@ -0,0 +1,30 @@
+/*
+TÓPICO 4
+3 PROPRIEDADES DE STRING: length, toUpperCase(), charAt();
+5 metotodos de elemento DOM: querySelector(), getAttribute(), innerText, innerHTML, addEventListenes(),
+3 metodos de conversão de números: parseInt(), parserFloat(), Number() 
+*/ 
+
+/*
+TÓPICO 5 
+exemplo 01
+ISSO ACONTECE PORQUE O console.log NÃO IMPRIME VAR, teria quue imprimir "COR" 
+*/ 
+somarDois(4);
+dividirDois(6)
+function somarDois(x){
+    dois=2;
+    return x+dois;
+}
+function dividirDois(x){
+    return x/ dois;
+}
+somarDois(4);
+dividirDois(6); 
+/*ex3 Correta*/
+var numero = 50;
+for (let numero = 0; numero <10; numero++) {
+    console.log(numero);
+}
+const  total =10*numero;
+console.log(total);
